def47bd9cf30f3463c30c57382a592588f572d50
liucjray <liucjray@gmail.com>
2018-07-21 10:59:40 +0800

    修改 description

--- resources/views/Dictionary/index.blade.php
+++ resources/views/Dictionary/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Translate-shell</title>
-    <meta name="description" content="A stream video player on MEGA.">
+    <meta name="description" content="煞氣a翻譯機">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 </head>
